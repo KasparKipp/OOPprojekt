f3a726d4f813127eacc2d3bfd481fc901d0a6bf9
Kaspar <kaspar.kipp@gmail.com>
2022-03-27 01:33:25 +0200

    Made this project a spring application

---
+++ src/main/java/com/projekt/Ridebuddy/RidebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.projekt.Ridebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidebuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt/Ridebuddy/RidebuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.Ridebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidebuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

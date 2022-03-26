ffbbbad3c5137fce8794871f441a237e1b4db550
Kaspar <kaspar.kipp@gmail.com>
2022-03-26 22:15:28 +0200

    issue #2: model for ride data

---
+++ src/main/java/com/projekt/rides/model/Ride.java
@@ -0,0 +1,57 @@
+package com.projekt.rides.model;
+
+import java.util.Date;
+
+public class Ride implements Comparable<Ride>{
+    private final Date date;
+    private final String address;
+    private final double price;
+    private final double cancellationFee;
+    private final double tip;
+    private final char valuuta; //What currency is used
+    private final boolean paymentMethod; //True if cash, else false
+    private final int rideDistance;//in km
+
+    public Ride(Date date,
+                String address,
+                double price,
+                double cancellationFee,
+                double tip, char valuuta,
+                boolean paymentMethod,
+                int rideDistance) {
+        this.date = date;
+        this.address = address;
+        this.price = price;
+        this.cancellationFee = cancellationFee;
+        this.tip = tip;
+        this.valuuta = valuuta;
+        this.paymentMethod = paymentMethod;
+        this.rideDistance = rideDistance;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getRideIncome() {
+        return price + cancellationFee + tip;
+    }
+
+    public int getRideDistance() {
+        return rideDistance;
+    }
+
+    @Override
+    public int compareTo(Ride o) {
+        return this.getDate().compareTo(o.getDate());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ src/main/java/com/projekt/rides/service/RideMonth.java
@@ -0,0 +1,5 @@
+package com.projekt.rides.service;
+
+public class RideMonth {
+    //TODO
+}
